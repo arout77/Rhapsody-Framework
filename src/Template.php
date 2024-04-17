03db9467045a1416b6cb50e050e79fa183dc807a
Andrew Rout <andrew_rout@yahoo.com>
2024-04-17 09:51:18 -0500

    Added custom Pagination middleware. Lots of code cleanup.

--- src/Template.php
+++ src/Template.php
@@ -1,47 +1,50 @@
 <?php
 
 namespace Src;
-use \Twig\Environment;
-use \Twig\Loader\ArrayLoader;
-use \Twig\Loader\FilesystemLoader;
 use \Pimple\Container as ServiceLocator;
 
-class Template {
-
-	protected $_settings;
-	protected $app;
-	public string $path_to_template_files;
-	public string $path_to_cache;
-	public $twigLoader;
-	public $twigEnv;
+class Template
+{
 	public $current_page;
 
-	public function __construct(ServiceLocator $app) {
-		$this->app = $app;
-		$this->_settings = $app['config'];
-		$this->path_to_template_files = $this->_settings->setting('template_folder');
-		$this->path_to_cache = $this->_settings->setting('var_path').'/cache/';
+	public string $path_to_cache;
 
-		$this->twigLoader = new \Twig\Loader\FilesystemLoader($this->path_to_template_files);
-		$this->twigEnv = new \Twig\Environment($this->twigLoader, [
+	public string $path_to_template_files;
+
+	public $twigEnv;
+
+	public $twigLoader;
+
+	protected $_settings;
+
+	protected $app;
+
+	public function __construct( ServiceLocator $app )
+	{
+		$this->app                    = $app;
+		$this->_settings              = $app['config'];
+		$this->path_to_template_files = $this->_settings->setting( 'template_folder' );
+		$this->path_to_cache          = $this->_settings->setting( 'var_path' ) . '/cache/';
+
+		$this->twigLoader = new \Twig\Loader\FilesystemLoader( $this->path_to_template_files );
+		$this->twigEnv    = new \Twig\Environment( $this->twigLoader, [
 			'auto_reload' => true,
 			'cache' => $this->path_to_cache,
 			'debug' => true,
-			
-		]);
+
+		] );
 		// Add var_dump to template files
 		$this->twigEnv->addExtension( new \Twig\Extension\DebugExtension() );
 		// Pass some global vars to Twig templates
-		$this->twigEnv->addGlobal('base_url', $this->_settings->setting('site_url'));
-		$this->twigEnv->addGlobal('current_page', $app['router']->controller);
-		$this->twigEnv->addGlobal('controllers_path', $this->_settings->setting('controllers_path'));
-		$this->twigEnv->addGlobal('views_path', $this->_settings->setting('template_folder'));
-		$this->twigEnv->addGlobal('debug_mode', $this->_settings->setting('debug_mode'));
-		
+		$this->twigEnv->addGlobal( 'base_url', $this->_settings->setting( 'site_url' ) );
+		$this->twigEnv->addGlobal( 'current_page', $app['router']->controller );
+		$this->twigEnv->addGlobal( 'controllers_path', $this->_settings->setting( 'controllers_path' ) );
+		$this->twigEnv->addGlobal( 'views_path', $this->_settings->setting( 'template_folder' ) );
+		$this->twigEnv->addGlobal( 'debug_mode', $this->_settings->setting( 'debug_mode' ) );
 	}
 
-	public function render($template_file, $vars = [])
+	public function render( $template_file, $vars = [] )
 	{
-		echo $this->twigEnv->render($template_file, $vars);
+		echo $this->twigEnv->render( $template_file, $vars );
 	}
 }
