9ba7eb634c5d932f80849afeac4655ed799bd9a1
Andrew Rout <32005538+arout77@users.noreply.github.com>
2026-05-26 00:02:59 -0400

    Update config.php

--- config.php
+++ config.php
@@ -34,8 +34,8 @@ return [
         'password' => $_ENV['REDIS_PASSWORD'] ?? null,
     ],
     'database'    => [
-        'host'     => $_ENV['DB_HOST'] ?? '127.0.0.1',
-        'port'     => $_ENV['DB_PORT'] ?? 3306,
+        'host'     => $_ENV['DB_HOST'],
+        'port'     => $_ENV['DB_PORT'],
         'dbname'   => $_ENV['DB_NAME'],
         'user'     => $_ENV['DB_USER'],
         'password' => $_ENV['DB_PASS'],
